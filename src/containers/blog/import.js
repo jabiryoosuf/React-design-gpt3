aac2aa9c5ca902d7b54a837bbc8fd94d64dd9ebc
jabiryoosuf <jabiribnuyoosuf@gmail.com>
2023-01-05 10:34:02 +0530

    first commit

---
+++ src/containers/blog/import.js
@@ -0,0 +1,13 @@
+import blog01 from '../../assets/Rectangle 22.png';
+import blog02 from '../../assets/Rectangle 23.png';
+import blog03 from '../../assets/Rectangle 24.png';
+import blog04 from '../../assets/Rectangle 25.png';
+import blog05 from '../../assets/Rectangle 26.png';
+
+export {
+  blog01,
+  blog02,
+  blog03,
+  blog04,
+  blog05,
+};
